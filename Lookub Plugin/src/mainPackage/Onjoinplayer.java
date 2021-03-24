a4cfc2f04d5bc1cf1c9a6563ed7f6bd40e56da6a
TyoIV <tomfosse44@gmail.com>
2021-03-24 17:26:13 +0100

    Ver. 0.0.2

---
+++ Lookub Plugin/src/mainPackage/Onjoinplayer.java
@@ -0,0 +1,13 @@
+package mainPackage;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Onjoinplayer extends JavaPlugin {
+	
+	public Onjoinplayer() {
+		// TODO Auto-generated constructor stub
+		
+		System.out.println("coucou");
+	}
+
+}
--- Lookub Plugin/src/mainPackage/mainClass.java
+++ Lookub Plugin/src/mainPackage/mainClass.java
@@ -9,7 +9,7 @@ import org.bukkit.scheduler.BukkitRunnable;
 import mainPackage.commands.Commands;
 import mainPackage.commands.CreeperSpawn;
 public class mainClass extends JavaPlugin {
-
+	
 	@SuppressWarnings("deprecation")
 	@Override
 	public void onEnable() {
@@ -18,8 +18,8 @@ public class mainClass extends JavaPlugin {
 		System.out.println("The plugin was launch sucessful");
 		getCommand("alert").setExecutor(new Commands());	
 		getCommand("money").setExecutor(new Commands());
-		getCommand("upgrade").setExecutor(new Commands());
 		getCommand("creeper").setExecutor(new CreeperSpawn(this));
+	
 	
 			String[] messages = {"Noubliez pas de nous suivre sur le Twitter du serveur https://twitter.com/lookubserver", "Rejoignez notre discord https://discord.gg/v4FepDCTGa"};
 			Bukkit.getScheduler().runTaskTimer(this, new BukkitRunnable() {
